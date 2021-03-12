849cc71715f06c33d593556119e7fd9cc631740b
Łukasz Latusik <latusikl@gmail.com>
2021-03-12 22:22:49 +0100

    Add base of menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'photo-catalog';
+  isExpanded: boolean = false;
+  isShowing: boolean = false;
+
+  toggleMenu() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+  onMouseEnter() {
+    if (!this.isExpanded) {
+      this.isShowing = true;
+    }
+  }
+  onMouseExit() {
+    if (!this.isExpanded) {
+      this.isShowing = false;
+    }
+  }
+
+  isSideNavFullWidth(): boolean {
+    return this.isExpanded || this.isShowing;
+  }
 }
