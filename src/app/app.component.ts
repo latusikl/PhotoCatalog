e70ac0f948e1c211e75cc01b0a40c9e00b44db2e
Suchar230 <dsj230@gmail.com>
2021-03-14 15:34:53 +0100

    Lint code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,23 @@
-import { Component } from "@angular/core";
-import { DirectoryService } from "./service/directory.service";
+import { Component } from '@angular/core';
+import { DirectoryService } from './service/directory.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private directoryService: DirectoryService) {
-    this.isExpanded = true;
-  }
+    constructor(private directoryService: DirectoryService) {
+        this.isExpanded = true;
+    }
 
-  isExpanded: boolean;
+    isExpanded: boolean;
 
-  toggleMenu() {
-    this.isExpanded = !this.isExpanded;
-  }
+    toggleMenu(): void {
+        this.isExpanded = !this.isExpanded;
+    }
 
-  chooseFolder() {
-    this.directoryService.callForDirectoryChoice();
-  }
+    chooseFolder(): void {
+        this.directoryService.callForDirectoryChoice();
+    }
 }
--- src/app/service/directory.service.ts
+++ src/app/service/directory.service.ts
@@ -1,22 +1,22 @@
-import { Injectable } from "@angular/core";
-import { ElectronService } from "ngx-electron";
-import { BehaviorSubject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+    providedIn: 'root',
 })
 export class DirectoryService {
-  constructor(private electronService: ElectronService) {}
+    constructor(private electronService: ElectronService) {}
 
-  currentDirectory = new BehaviorSubject<string>('');
+    currentDirectory = new BehaviorSubject<string>('');
 
-  callForDirectoryChoice() {
-    console.log(this.electronService.isElectronApp);
-    console.log(this.electronService.ipcRenderer);
-    this.electronService.ipcRenderer.send("select-dir");
+    callForDirectoryChoice(): void {
+        console.log(this.electronService.isElectronApp);
+        console.log(this.electronService.ipcRenderer);
+        this.electronService.ipcRenderer.send('select-dir');
 
-    this.electronService.ipcRenderer.on("dir-selected", (ev, dir) => {
-      this.currentDirectory.next(dir);
-    });
-  }
+        this.electronService.ipcRenderer.on('dir-selected', (ev, dir) => {
+            this.currentDirectory.next(dir);
+        });
+    }
 }
