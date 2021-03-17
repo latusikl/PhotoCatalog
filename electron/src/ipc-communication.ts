efea7bfee2c5a186e7c0ac4d8a50565bf832e133
syphzur <krzysztof.bol@wp.pl>
2021-03-17 23:40:25 +0100

    code refactor

--- electron/src/ipc-communication.ts
+++ electron/src/ipc-communication.ts
@@ -15,40 +15,42 @@ class IpcCommunication implements IpcRegister {
 function addSelectDirHandler(window: BrowserWindow | null): void {
     if (!window) {
         console.error('Window was null!');
-    } else {
-        ipcMain.on('select-dir', async () => {
-            const result = await dialog.showOpenDialog(window, {
-                properties: ['openDirectory'],
-            });
-            window.webContents.send('dir-selected', result.filePaths);
-        });
+        return;
     }
+
+    ipcMain.on('select-dir', async () => {
+        const result = await dialog.showOpenDialog(window, {
+            properties: ['openDirectory'],
+        });
+        window.webContents.send('dir-selected', result.filePaths);
+    });
 }
 
 function getImagePathsFromDir(window: BrowserWindow | null): void {
     if (!window) {
         console.error('Window was null!');
-    } else {
-        ipcMain.on('get-images', (ev, par: string[]) => {
-            const dir = par[0];
-            if (!dir) {
-                window.webContents.send('images-found', []);
-            } else {
-                fs.readdir(dir, (err, files) => {
-                    if (!!err) {
-                        console.error(err);
-                    }
-                    files = files
-                        .map((name) => path.join(dir, name))
-                        .filter((imgDir) => {
-                            const imgChunk = readChunk.sync(imgDir, 0, 12);
-                            return imageType(imgChunk)?.mime.startsWith('image') ?? false;
-                        });
-                    window.webContents.send('images-found', files);
-                });
-            }
-        });
+        return;
     }
+
+    ipcMain.on('get-images', (ev, par: string[]) => {
+        const dir = par[0];
+        if (!dir) {
+            window.webContents.send('images-found', []);
+        } else {
+            fs.readdir(dir, (err, files) => {
+                if (!!err) {
+                    console.error(err);
+                }
+                files = files
+                    .map((name) => path.join(dir, name))
+                    .filter((imgDir) => {
+                        const imgChunk = readChunk.sync(imgDir, 0, 12);
+                        return imageType(imgChunk)?.mime.startsWith('image') ?? false;
+                    });
+                window.webContents.send('images-found', files);
+            });
+        }
+    });
 }
 
 export default new IpcCommunication();
