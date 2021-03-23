1c8831687cdb96e91937531e813239c4ce4210ac
Łukasz Latusik <latusikl@gmail.com>
2021-03-23 19:43:50 +0100

    Add listener to electron dev runner

--- electron/src/electron.dev.ts
+++ electron/src/electron.dev.ts
@@ -34,6 +34,12 @@ app.whenReady().then(() => {
     createWindow();
 });
 
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
 app.on('activate', () => {
     if (window === null) {
         createWindow();
